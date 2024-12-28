a9d113acddd8a74e701e6ae94297ef84309c76ce
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2024-12-29 04:17:51 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ import LoadingSpinner from './components/common/LoadingSpinner';
 // Lazy load pages
 const Home = React.lazy(() => import('./pages/Home'));
 const About = React.lazy(() => import('./pages/About'));
-const Services = React.lazy(() => import('./pages/Services'));
-const Portfolio = React.lazy(() => import('./pages/Portfolio'));
-const Blog = React.lazy(() => import('./pages/Blog'));
 const Contact = React.lazy(() => import('./pages/Contact'));
 
 function App() {
@@ -22,9 +19,6 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/portfolio" element={<Portfolio />} />
-              <Route path="/blog" element={<Blog />} />
               <Route path="/contact" element={<Contact />} />
             </Routes>
           </AnimatePresence>
